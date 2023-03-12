665425202db977aeab959d05ed945a6880acc19f
Hossain Chowdhury <111006379+hossain0011@users.noreply.github.com>
2023-03-12 12:27:42 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("hiddenBlock").style.right = "0px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("hiddenBlock").style.right = "-400px";
+  }
